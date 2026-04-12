b1ea47897dd6a268880678b22c85f1c4db957e99
lz0933032026-pixel <lz0933032026@unab.edu.sv>
2026-04-12 11:25:20 -0600

    parcial 02 fundamentos de programacion ejercicio9 Reemplaza una frase específica y convierte todo el texto a mayúsculas

---
+++ Parcial II/ejercicio9.py
@@ -0,0 +1,12 @@
+# 1. Tomar la cadena
+texto = "Any time y Anytime"
+
+# 2. Reemplazar todas las apariciones de "Any time" (con espacio) por "Always"
+texto_reemplazado = texto.replace("Any time", "Always")
+
+# 3. Convertir todo el texto resultante a mayúsculas
+texto_final = texto_reemplazado.upper()
+
+# Mostrar resultados
+print("Texto reemplazado:", texto_reemplazado)
+print("Texto final:", texto_final)
